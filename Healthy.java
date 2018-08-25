9cba8066a8e5c3413c87dab13dd889bb0a45de28
unknown <danijel.cokara@gmail.com>
2018-08-25 13:10:25 +0200

    lala

---
+++ Healthy.java
@@ -0,0 +1,30 @@
+
+
+public class Healthy
+{  
+ 
+   public static void main(String args[])
+   {
+      private String name;
+      private char gender;
+      private double weightpd;
+      private double weightkg;
+      private double heightinc;
+      private double heightcm;
+      private int age;
+      private int actlvl;
+      
+   public Healthy(String name, char gender, double weightkg, double heightcm, int age, int actlvl)
+   {
+      this.name = name;
+      this.gender = gender;
+      this.weightkg = weightkg;
+      this.heightcm = heightcm;
+      this.age = age;
+      this.actlvl = actlvl;      
+   }
+   
+   public double 
+   }
+   
+}  
